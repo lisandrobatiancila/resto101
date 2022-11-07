f462e85a5982aa217a03f0ae47934dfb8eb0312f
lisandro.batiancila <lisandro.batiancila@gmail.com>
2022-11-07 15:29:55 +0900

    adding target _blank to anchor

--- static/footer.php
+++ static/footer.php
@@ -4,17 +4,23 @@
         <div class="special-thanks">
             <h3 class="text-capitalize">special thanks</h3>
             <ul>
-                <li><a href="https://www.flaticon.com/">flaticon</a></li>
-                <li><a href="https://www.freepik.com/">freepik</a></li>
+                <li><a href="https://www.flaticon.com/"
+                    target="_blank">flaticon</a></li>
+                <li><a href="https://www.freepik.com/"
+                    target="_blank">freepik</a></li>
             </ul>
         </div>
         <div class="my-projects">
             <h3 class="text-capitalize">my projects</h3>
             <ul>
-                <li><a href="https://recipe101.herokuapp.com">recipe</a></li>
-                <li><a href="https://resto-mock.herokuapp.com">restaurant</a></li>
-                <li><a href="https://mobile-mock101.herokuapp.com">mobile-mock</a></li>
-                <li><a href="https://github.com/lisandrobatiancila">My Github</a></li>
+                <li><a href="https://recipe101.herokuapp.com"
+                    target="_blank">recipe</a></li>
+                <li><a href="https://resto-mock.herokuapp.com"
+                    target="_blank">restaurant</a></li>
+                <li><a href="https://mobile-mock101.herokuapp.com"
+                    target="_blank">mobile-mock</a></li>
+                <li><a href="https://github.com/lisandrobatiancila"
+                    target="_blank">My Github</a></li>
             </ul>
         </div>
 
